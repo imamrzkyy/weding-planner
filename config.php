64a213c8bdef2f7a07c99a50bb02f23a1c77406b
imamrzkyy <imamrizki475@gmail.com>
2026-06-16 20:38:02 +0700

    (feat) add deployment config

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$password = "";
-$database = "wo_web";
+$host = getenv('DB_HOST') ? : "127.0.0.1";
+$user = getenv('DB_USERNAME') ? : "root";
+$password = getenv('DB_PASSWORD') ? : "";
+$database = getenv('DB_DATABASE') ? : "wo_web";
+$port = getenv('DB_PORT') ? : 3306;
 
-$conn = new mysqli($host, $user, $password, $database);
+
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
